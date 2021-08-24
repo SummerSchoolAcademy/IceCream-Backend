c5824faa758917279d2225a9d162e54772323800
David Albert-Constantin <d.albert015@yahoo.com>
2021-08-24 13:41:16 +0300

    hello world

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
@@ -8,6 +8,7 @@ public class IcecreamshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IcecreamshopApplication.class, args);
+		System.out.println("Hello world!");
 	}
 
 }
