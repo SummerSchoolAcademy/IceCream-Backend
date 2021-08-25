e01d498da5b2ea40cec18308b3bbb5188c510c7b
GhergheTomy <63097760+GhergheTomy@users.noreply.github.com>
2021-08-25 19:21:06 +0300

    Added Swagger Configuration

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
@@ -2,12 +2,17 @@ package com.summerschool.icecreamshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
 @SpringBootApplication
 public class IcecreamshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IcecreamshopApplication.class, args);
 	}
+
+
 
 }
