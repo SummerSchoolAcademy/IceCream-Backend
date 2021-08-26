611be3a9702c8cf0475c135fd9c57f9d4d0e6713
Cosmin Păunică <cosmin_paunica@yahoo.com>
2021-08-26 20:55:38 +0300

    get category endpoint (with all additional classes)

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/service/CategoryService.java
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/service/CategoryService.java
@@ -5,10 +5,18 @@ import com.summerschool.icecreamshop.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import com.summerschool.icecreamshop.model.Category;
+import com.summerschool.icecreamshop.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 @Service
 public class CategoryService {
 
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
@@ -18,4 +26,9 @@ public class CategoryService {
     public Category add(Category category) {
         return categoryRepository.save(category);
     }
+
+    public Optional<Category> get(Long id) {
+        return categoryRepository.findById(id);
+    }
+
 }
