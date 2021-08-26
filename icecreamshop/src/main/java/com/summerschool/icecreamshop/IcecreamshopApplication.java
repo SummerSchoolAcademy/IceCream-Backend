891683ed66ea214c551ccef83b100a843ec4738d
GhergheTomy <63097760+GhergheTomy@users.noreply.github.com>
2021-08-26 09:50:10 +0300

    Spaces removed

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @EnableSwagger2
 @SpringBootApplication
 public class IcecreamshopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IcecreamshopApplication.class, args);
 	}
