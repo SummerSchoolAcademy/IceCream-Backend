bcc7db319ffdfe25eb999c71a70f90cba1895379
AlexStan20 <alexnstan14@gmail.com>
2021-08-27 19:23:08 +0300

    test unitar controller

--- icecreamshop/src/test/java/com/summerschool/icecreamshop/controller/RateControllerTest.java
+++ icecreamshop/src/test/java/com/summerschool/icecreamshop/controller/RateControllerTest.java
@@ -2,11 +2,16 @@ package com.summerschool.icecreamshop.controller;
 
 import com.summerschool.icecreamshop.model.Product;
 import com.summerschool.icecreamshop.model.Rate;
+import com.summerschool.icecreamshop.model.RateDTO;
 import com.summerschool.icecreamshop.repository.RateRepository;
 import com.summerschool.icecreamshop.service.RateService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -20,6 +25,12 @@ public class RateControllerTest {
     @Mock
     private RateService rateService;
 
+    @Mock
+    private ModelMapper modelMapper;
+
+    @Mock
+    private RateDTO rateDTO;
+
     @Test
     public void testUpdateProductRating(Long id) {
 
@@ -27,18 +38,11 @@ public class RateControllerTest {
         rate.setId(1L);
         rate.setRate(5);
 
-        when(rateService.update(rate,new Product())).thenReturn(rate);
-        Rate responseRate = rateService.update(rate,new Product());
-        assertNotNull(responseRate);
-        assertEquals(5,responseRate.getRate());
+        Mockito.when(modelMapper.map(rateDTO,Rate.class)).thenReturn(rate);
+        Mockito.when(rateService.update(rate,new Product())).thenReturn(rate);
+        ResponseEntity<RateDTO> r = rateController.update(rate.getId(),rateDTO);
+        assertEquals(HttpStatus.OK, r.getStatusCode());
+        assertNotNull(r);
 
-        Rate rate2 = new Rate();
-        rate2.setId(1L);
-        rate2.setRate(null);
-
-        when(rateService.update(rate2,new Product())).thenReturn(rate2);
-        Rate responseRate2 = rateService.update(rate,new Product());
-        assertNotNull(responseRate2);
-        assertEquals(5,responseRate.getRate());
     }
 }
