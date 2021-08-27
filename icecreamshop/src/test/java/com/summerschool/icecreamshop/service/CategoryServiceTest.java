295fe46d20f5c5d8edf4aa97de06d3e1ecf9fe83
Cosmin Păunică <cosmin_paunica@yahoo.com>
2021-08-27 10:39:56 +0300

    added unit tests for CategoryService

--- icecreamshop/src/test/java/com/summerschool/icecreamshop/service/CategoryServiceTest.java
+++ icecreamshop/src/test/java/com/summerschool/icecreamshop/service/CategoryServiceTest.java
@@ -12,8 +12,9 @@ import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CategoryServiceTest {
@@ -26,9 +27,15 @@ public class CategoryServiceTest {
 
     Category category;
 
+    Category categoryInDb;
+
     @Before
     public void setUp() {
         category = new Category(5L, "Test Name", "ceva", new ArrayList<Product>());
+        categoryInDb = new Category();
+        categoryInDb.setId(1L);
+        categoryInDb.setName("Gelato");
+        categoryInDb.setDescription("Cool yourself on a hot summer day with our gelato!");
     }
 
     @Test
@@ -36,4 +43,20 @@ public class CategoryServiceTest {
         Mockito.when(categoryRepository.save(category)).thenReturn(category);
         assertEquals(category.getName(), categoryService.add(category).getName());
     }
+
+    @Test
+    public void testGetCategoryByValidId() {
+        Mockito.when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(categoryInDb));
+        Category actualCategory = categoryService.get(1L).get();
+
+        assertEquals(categoryInDb.getId(), actualCategory.getId());
+        assertEquals(categoryInDb.getName(), actualCategory.getName());
+        assertEquals(categoryInDb.getDescription(), actualCategory.getDescription());
+    }
+
+    @Test
+    public void testGetCategoryByInvalidId() {
+        assertTrue(categoryService.get(-1L).isEmpty());
+    }
+
 }
