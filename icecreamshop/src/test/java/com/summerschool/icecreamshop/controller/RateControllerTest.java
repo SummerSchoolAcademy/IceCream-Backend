56e6931edb9c7a96dd5c3fb6a9edffe3eacd6c14
AlexStan20 <alexnstan14@gmail.com>
2021-08-30 10:31:45 +0300

    rezolvare conflict

--- icecreamshop/src/test/java/com/summerschool/icecreamshop/controller/RateControllerTest.java
+++ icecreamshop/src/test/java/com/summerschool/icecreamshop/controller/RateControllerTest.java
@@ -5,18 +5,22 @@ import com.summerschool.icecreamshop.model.Rate;
 import com.summerschool.icecreamshop.model.RateDTO;
 import com.summerschool.icecreamshop.repository.RateRepository;
 import com.summerschool.icecreamshop.service.RateService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class RateControllerTest {
 
     @InjectMocks
@@ -31,14 +35,20 @@ public class RateControllerTest {
     @Mock
     private RateDTO rateDTO;
 
-    @Test
-    public void testUpdateProductRating(Long id) {
-
+    @Before
+    public void setUp(){
         Rate rate = new Rate();
         rate.setId(1L);
         rate.setRate(5);
 
-        Mockito.when(modelMapper.map(rateDTO,Rate.class)).thenReturn(rate);
+        RateDTO rateDTO = new RateDTO();
+        rateDTO.setId(1L);
+        rateDTO.setRate(4);
+    }
+
+    @Test
+    public void testUpdateProductRating(Long id) {
+
         Mockito.when(rateService.update(rate,new Product())).thenReturn(rate);
         ResponseEntity<RateDTO> r = rateController.update(rate.getId(),rateDTO);
         assertEquals(HttpStatus.OK, r.getStatusCode());
