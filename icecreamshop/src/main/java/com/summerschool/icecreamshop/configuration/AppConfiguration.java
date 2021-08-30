640d64e8d37c78b32570128a9197d6927505d986
GhergheTomy <63097760+GhergheTomy@users.noreply.github.com>
2021-08-30 14:05:16 +0300

    Create Product

---
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/configuration/AppConfiguration.java
@@ -0,0 +1,38 @@
+package com.summerschool.icecreamshop.configuration;
+
+import com.summerschool.icecreamshop.dto.ProductDTO;
+import com.summerschool.icecreamshop.model.Product;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+
+        Converter<ProductDTO, Product> productConverter = context -> {
+            ProductDTO s = context.getSource();
+            Product d = new Product();
+            d.setId(s.getId());
+            d.setTitle(s.getTitle());
+            d.setShortDescription(s.getShortDescription());
+            d.setLongDescription(s.getLongDescription());
+            d.setIngredients(s.getIngredients());
+            d.setQuantity(s.getQuantity());
+            d.setAlergens(s.getAlergens());
+            d.setPrice(s.getPrice());
+            d.setCurrency(s.getCurrency());
+            d.setPhoto_urls(s.getPhoto_urls());
+            d.setType(s.getType());
+            return d;
+        };
+
+        modelMapper.addConverter(productConverter);
+
+        return modelMapper;
+    }
+}
