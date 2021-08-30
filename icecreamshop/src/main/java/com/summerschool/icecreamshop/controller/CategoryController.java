94ee042ecc7aaaa69a7d5833e677bd57eafdd3bc
Teodora-Samira Manole <manolesamira99@gmail.com>
2021-08-30 03:10:06 +0300

    Patch requests now supported. Bug: product list lost

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/CategoryController.java
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/CategoryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
-import static com.summerschool.icecreamshop.exception.Constants.*;
+import static com.summerschool.icecreamshop.exception.Constants.CATEGORY_NOT_FOUND;
 
 @RestController
 @RequestMapping(path = "/categories")
