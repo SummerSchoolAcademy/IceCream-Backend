cab1ec9b501b6bc62b68cda337cf931fa5172638
Avra <andreiavramescu001@yahoo.com>
2021-08-31 15:57:58 +0300

    Update2

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/exception/InvalidPage.java
+++
@@ -1,8 +0,0 @@
-package com.summerschool.icecreamshop.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "All the copies of the book are already borrowed.")
-public class InvalidPage {
-}
