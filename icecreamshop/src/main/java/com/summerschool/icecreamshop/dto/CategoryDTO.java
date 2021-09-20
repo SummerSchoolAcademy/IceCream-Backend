66aa1eacfcdb3813d68353583b514cf46bedac33
Avra <andreiavramescu001@yahoo.com>
2021-09-20 16:57:07 +0300

    Changed up

--- icecreamshop/src/main/java/com/summerschool/icecreamshop/dto/CategoryDTO.java
+++ icecreamshop/src/main/java/com/summerschool/icecreamshop/dto/CategoryDTO.java
@@ -13,13 +13,19 @@ public class CategoryDTO {
 
     private List<ProductDTO> products;
 
+    private List<String> categoryPhotoBig;
+
+    private List<String> categoryPhotoMedium;
+
     public CategoryDTO() {}
 
-    public CategoryDTO(Long id, String name, String description, List<ProductDTO> products) {
+    public CategoryDTO(Long id, String name, String description, List<ProductDTO> products, List<String> categoryPhotoBig, List<String> categoryPhotoMedium) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.products = products;
+        this.categoryPhotoBig=categoryPhotoBig;
+        this.categoryPhotoMedium=categoryPhotoMedium;
     }
 
     public void setId(Long id) {
@@ -38,6 +44,12 @@ public class CategoryDTO {
         this.products = products;
     }
 
+    public void setCategoryPhotoBig(List<String> categoryPhotoBig) {
+        this.categoryPhotoBig = categoryPhotoBig;
+    }
+
+    public void setCategoryPhotoMedium(List<String> categoryPhotoMedium) {this.categoryPhotoMedium = categoryPhotoMedium;}
+
     public Long getId() {
         return id;
     }
@@ -52,5 +64,13 @@ public class CategoryDTO {
 
     public List<ProductDTO> getProducts() {
         return products;
+    }
+
+    public List<String> getCategoryPhotoBig() {
+        return categoryPhotoBig;
+    }
+
+    public List<String> getCategoryPhotoMedium() {
+        return categoryPhotoMedium;
     }
 }
